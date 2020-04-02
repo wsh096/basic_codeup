ce821b04fd89003b47729f8d4037bf6273e6ab7f
wsh096 <wsh096@gmail.com>
2020-04-02 06:04:29 +0000

    마지막 출력 때 자리수에 따르게 사용하기

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 
 int main(){
 
-int h, m;
-scanf("%d:%d",&h ,&m);
-printf("%d:%d",h,m);
+int y, m, d;
+scanf("%d.%d.%d",&y,&m,&d);
+printf("%04d.%02d.%02d",y,m, d);
 }
+
+//마지막 출력 때 %02를 사용하거나 그 이상의 숫자를 사용해서 yyyy.mm.dd의 형태를 지정해줄 수 있다.
