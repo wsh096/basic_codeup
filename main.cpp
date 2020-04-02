f49c87b1f18747405f4d4610444529278f6a2dbb
wsh096 <wsh096@gmail.com>
2020-04-02 05:33:58 +0000

    두 개의 문자값 입출력 scanf 차이가 있으므로 유의

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 
 int main(){
 
-int a, b;
-scanf("%d%d", &a, &b);
-printf("%d %d", a, b);
+char a, b;
+scanf("%c %c", &a, &b); 
+printf("%c %c", b, a);
 
 }
 
+//여기서는 %c%c로 붙여쓰게 되면 문자가 구분이 안 되기 때문에 입력 값도 떨어뜨려서 해준다.!
+//%d%d는 가능
