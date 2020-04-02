39d2d19b039ba183e126bb87f6556988f520f962
wsh096 <wsh096@gmail.com>
2020-04-02 05:16:57 +0000

    정수입력과 출력 주소값으로 n을 넣네

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
 int main() {
-  std::cout << "\u250C\u252C\u2510\n\n\u251c\u253c\u2524\n\n\u2514\u2534\u2518" ;
+  int n;
+  scanf("%d", &n);
+  printf("%d", n);
+  
+  return 0;
 }
