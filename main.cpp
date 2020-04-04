151a4ebd13804e8c271a647555cd6df23fb3d5a7
wsh096 <wsh096@gmail.com>
2020-04-04 04:33:37 +0000

    1027번 날짜 거꾸로 출력하기

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main(){
-  int a,b,c,d,e;
-  scanf("%1d%1d%1d%1d%1d",&a,&b,&c,&d,&e);
+  int a,b,c;
+  scanf("%d.%d.%d",&a,&b,&c);
   
-  printf("[%d]\n[%d]\n[%d]\n[%d]\n[%d]\n",a*10000,b*1000,c*100,d*10,e*1);
+  printf("%02d-%02d-%04d",c,b,a);
   }
 
 //문자로는 '\0', 아스키문자의 정수값은 0을 의미하며 NULL 로도 사용할 수 있다.
