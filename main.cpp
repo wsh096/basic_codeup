2a5eb4ab7f61629f2aa6d12ada296b515ea2fc21
wsh096 <wsh096@gmail.com>
2020-04-04 05:15:05 +0000

    1038_1073741824수의 범위 long int

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main(){
-  int a;
-  scanf("%d",&a);
-  printf("%c",a);
+  long int a, b;
+  scanf("%ld %ld",&a, &b);
+  printf("%ld",a + b);
   }
 
 //아스키 코드값이 정수값으로 출력하면 정수값이 나옴
