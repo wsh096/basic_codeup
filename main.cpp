00f50d220182eb95f76325e0c02ed5c4c926df04
wsh096 <wsh096@gmail.com>
2020-04-04 05:06:06 +0000

    1037_ 십진수>아스키코드

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main(){
-  char a;
-  scanf("%c",&a);
-  printf("%d",a);
+  int a;
+  scanf("%d",&a);
+  printf("%c",a);
   }
 
 //아스키 코드값이 정수값으로 출력하면 정수값이 나옴
-A는65
+//A는65
