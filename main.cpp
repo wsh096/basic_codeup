faf7a052cfc5ab3634d401645e0148968efb0df7
wsh096 <wsh096@gmail.com>
2020-04-04 05:30:25 +0000

    1042_ 2147483647정수 간의 나눗셈 출력

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
 int main(){
-  int a;
-  scanf("%c",&a);
-  printf("%c", a+1);
+  long long int a, b;
+  scanf("%lld %lld",&a, &b);
+  printf("%lld", a/b);
 }
 
-//int나 char이나 상관없네
+//int a;
+//scanf("%d", &a);
+//printf("%f", (float)a);
+//와 같이 실행하면, 정수로 저장되어 있는 값을 실수형(float)으로 명시적으로 변환하여 출력한다.
+//어떤 값이나 결과의 데이터형을 강제로 바꾸는 것을 명시적 (데이터)형변환이라고 한다.
+
