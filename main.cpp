95610731b3f8e974804dea845368c9092683cc94
wsh096 <wsh096@gmail.com>
2020-04-04 05:20:01 +0000

    1040_negative transfer

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 
 int main(){
-  long long int a, b;
-  scanf("%lld %lld",&a, &b);
-  printf("%lld",a + b);
-  }
-
-//아스키 코드값이 정수값으로 출력하면 정수값이 나옴
-//A는65
+  int a;
+  scanf("%d",&a);
+  printf("%d", -a);
+}
