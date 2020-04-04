8dde61855dc68826c951cbbd535685ca1b624ab5
wsh096 <wsh096@gmail.com>
2020-04-04 04:36:36 +0000

    golang uint//cpp unsigned int 심지어 %u로 값입력에 따른 문자도 다름

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main(){
-  int a,b,c;
-  scanf("%d.%d.%d",&a,&b,&c);
+  unsigned int a;
+  scanf("%u",&a);
   
-  printf("%02d-%02d-%04d",c,b,a);
+  printf("%u",a);
   }
 
 //문자로는 '\0', 아스키문자의 정수값은 0을 의미하며 NULL 로도 사용할 수 있다.
