a56b8aaed16673ce87df465c465cca17842490d5
wsh096 <wsh096@gmail.com>
2020-04-04 04:25:58 +0000

    int a,b,c,d,e는 변수선언!

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 
 int main(){
-  char d[20];
-  scanf("%s",d);
-  for(int i=0; d[i] != '\0'; i++)
-  {
-  printf("\'%c\'\n",d[i]);
+  int a,b,c,d,e;
+  scanf("%1d%1d%1d%1d%1d",&a,&b,&c,&d,&e);
+  
+  printf("[%d]\n[%d]\n[%d]\n[%d]\n[%d]\n",a*10000,b*1000,c*100,d*10,e*1);
   }
-}
+
 //문자로는 '\0', 아스키문자의 정수값은 0을 의미하며 NULL 로도 사용할 수 있다.
 //!=아니다 기호
 //for문(초기식(문자타입 알려줘야 함;조건식;변환식)
