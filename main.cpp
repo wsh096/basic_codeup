f9bbef9a32dde2454d53f6a435a7e207ea8e46ae
wsh096 <wsh096@gmail.com>
2020-04-04 04:46:05 +0000

    1029 long float

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 
 int main(){
-  unsigned int a;
-  scanf("%u",&a);
+  double a;
+  scanf("%lf",&a);//long float
   
-  printf("%u",a);
+  printf("%.11lf",a);
   }
 
-//문자로는 '\0', 아스키문자의 정수값은 0을 의미하며 NULL 로도 사용할 수 있다.
-//!=아니다 기호
-//for문(초기식(문자타입 알려줘야 함;조건식;변환식)
