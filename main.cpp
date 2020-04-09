f08dd4465960e9ac97da4069b81a8b3e6d705ec5
wsh096 <wsh096@gmail.com>
2020-04-09 07:03:03 +0000

    1054_a&&b

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 int main()
 {
-    int a;
-    scanf("%d", &a);
-    printf("%d", !a );
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d", a&&b );
     }
 //논리연산자 !(not), &&(and), ||(or), xor은?
