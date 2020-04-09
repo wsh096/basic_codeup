248aa9e1819f3206285d43d8f6e4095145e8c434
wsh096 <wsh096@gmail.com>
2020-04-09 07:38:08 +0000

    1061_bitwise_a|b

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 int main(){
     int a, b;
     scanf("%d%d",&a, &b);
-    printf("%d", a&b);
+    printf("%d", a|b);
 }
 //비트단위연산자~(bitwise not), &(bitwise and), |(bitwise or), ^(bitwise xor),<<(bitwise left shift), >>(bitwise right shift)
 //
