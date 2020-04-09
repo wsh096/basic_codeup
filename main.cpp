bf3c23583e12abd29c6e9618a3e97c83f263dcd7
wsh096 <wsh096@gmail.com>
2020-04-09 07:17:15 +0000

    1057_ (a&&b)||(!a&&!b)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-int main()
-{
+int main(){
     int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d", (a&&!b)||(!a&&b) );
-    }
+    scanf("%d %d",&a,&b);
+    printf("%d", (!a&&!b)||(a&&b));
+}
 //논리연산자 !(not), &&(and), ||(or), xor은? (a&&!b)||(!a&&b)//복잡하게 되어 있네 golanf은 a^b임 쉬움
