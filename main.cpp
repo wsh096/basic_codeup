43250c982827d310c767fb5bb82f86175e6f4d23
wsh096 <wsh096@gmail.com>
2020-04-09 07:31:54 +0000

    1059_비트단위 연산자 ~a//-a-1///~-a->-n+1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 int main(){
-    int a, b;
-    scanf("%d %d",&a,&b);
-    printf("%d", !a&&!b);
+    int a;
+    scanf("%d",&a);
+    printf("%d", ~a);
 }
-//논리연산자 !(not), &&(and), ||(or), xor은? (a&&!b)||(!a&&b)//복잡하게 되어 있네 golanf은 a^b임 쉬움
+//비트단위연산자~(bitwise not), &(bitwise and), |(bitwise or), ^(bitwise xor),<<(bitwise left shift), >>(bitwise right shift)
+//~n = -n - 1//-n = ~n + 1 과 같은 관계로 표현된다.
+//예를 들어 1이 입력되었을 때 저장되는 1을 32비트 2진수로 표현하면        00000000 00000000 00000000 00000001 이고,
+//~1은 11111111 11111111 11111111 11111110 가 되는데 이는 -2를 의미한다.
+//이 관계를 그림으로 그려보면 마치 원형으로 수들이 상대적으로 배치된 것과 같다.
+
