fea46572ce80a6f1a0fd8374d263ec05ef503171
wsh096 <wsh096@gmail.com>
2020-04-09 06:04:26 +0000

    1047_//비트연산_ <<1 2배/ >>1 절반// <<2 4배/ >>2 25%_정수만 출력

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main(){
-  unsigned int a, b, c;
-  scanf("%d %d %d",&a, &b, &c);
-  printf("%d\n%.1f", a+b+c, (float)(a+b+c)/3);
+  int a;
+  scanf("%d",&a);
+  printf("%d", a>>1);
 }
 
-//정수를 실수로 바꿔주고 (float)/소수점 1째 자리까지 나오게 .1f 사용//평균 갯수만큼 괄호로 나누기
+//비트연산_ <<1 2배/ >>1 절반// <<2 4배/ >>2 25%_정수만 출력
