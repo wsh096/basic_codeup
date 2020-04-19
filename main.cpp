782dd44f7bada23a12d934c2ccc00fa1f4e0c190
wsh096 <wsh096@gmail.com>
2020-04-19 11:08:27 +0000

    1070_season_switch code //숫자는 따로 ''필여 없음!

--- main.cpp
+++ main.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 
 int main(){
-    char a;
-    scanf("%c",&a);
+    int a;
+    scanf("%d",&a);
     switch(a){
-        case 'A':
-        printf("best!!!");
+        case 3:
+        case 4:
+        case 5:
+        printf("spring");
         break;
-        case 'B':
-        printf("good!!");
+        case 6:
+        case 7:
+        case 8:
+        printf("summer");
         break;
-        case 'C':
-        printf("run!");
+        case 9:
+        case 10:
+        case 11:
+        printf("fall");
         break;
-        case 'D':
-        printf("slowly~");
+        case 12:
+        case 1:
+        case 2:
+        printf("winter");
         break;
         default:
-        printf("what?");
-        break;//있으나 없으나 차이 없음
+        printf("it is wrong. please, retry!");
+        //break;//있으나 없으나 차이 없음
             }
 }
 //A는 아스키코드로 65에 해당하기 때문에 스위치의 정수만 사용이 가능한 사용에서 주소를 이용해서 사용이 가능!
+//숫자는 따로 ''필여 없음!
