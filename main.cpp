d01d96433815a323ca798f72f0f465cbf11a818b
wsh096 <wsh096@gmail.com>
2020-04-19 11:43:56 +0000

    1071_goto와 레이어 쌓기 if의 위치에 따른 용법과 return함수로 함수 종료시 0을 입력해 주어야 함!

--- main.cpp
+++ main.cpp
@@ -2,32 +2,23 @@
 
 int main(){
     int a;
-    scanf("%d",&a);
-    switch(a){
-        case 3:
-        case 4:
-        case 5:
-        printf("spring");
-        break;
-        case 6:
-        case 7:
-        case 8:
-        printf("summer");
-        break;
-        case 9:
-        case 10:
-        case 11:
-        printf("fall");
-        break;
-        case 12:
-        case 1:
-        case 2:
-        printf("winter");
-        break;
-        default:
-        printf("it is wrong. please, retry!");
+    
+    reload:
+    
+    scanf("%d", &a);
+    if(a ==0) goto end;
+    printf("%d\n", a);
+    if(a!=0) goto reload;
+
+    end:
+    return 0;
+    
+    
+        
+         
+    
         //break;//있으나 없으나 차이 없음
-            }
+           
 }
 //A는 아스키코드로 65에 해당하기 때문에 스위치의 정수만 사용이 가능한 사용에서 주소를 이용해서 사용이 가능!
-//숫자는 따로 ''필여 없음!
+//숫자는 따로 ''필요 없음!
