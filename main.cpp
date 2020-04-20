d74e047c72e3a2f9daf09f3e20cccfd7df005947
wsh096 <wsh096@gmail.com>
2020-04-20 10:51:25 +0000

    1080 if() break;//는 종료 구문이다!

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+
 int main(){
-  int i, n ,s=0;
-  scanf("%d", &n);
-  for(i=0; ;i++){
-    s+= i;
-    if(s>=n) break;
-  }
-  printf("%d", i);
-  return 0;//없어도 됌
+    int sum=0;
+    int i,n;//합쳐서 int i, n, s=0;으로 써도됨!
+   scanf("%d", &n);
+   for(i=0; sum<n;i++){//sum<n이 없어도 알아서 종료할 시점을 밑의 브레이크가 찾아주므로 여백으로 ;  ;둬도 된다!
+     sum=sum+i;//sum+=i라고 써도 됨
+     if(sum>=n)
+     break;
+     
+     
+   
+   }
+   printf("%d",i);
 }
+
+//if() break;//는 종료 구문이다!
+
