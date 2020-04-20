8bdccae745994e8f1ba304ae20e2b482d280f99d
wsh096 <wsh096@gmail.com>
2020-04-20 09:42:24 +0000

    1078_짝수간의 합에서 if(i%2==0)sum=sum+i; for문 종료후 결과값 출력! for(i=0;i<=n;i++){       if(i%2==0)sum=sum+i;       //printf("%d\n",sum);전부 과정을 다 보여줌!     }printf("%d",sum);

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 int main(){
-    int i,n;
+    int sum=0;
+    int i, n;
     scanf("%d", &n);
     for(i=0;i<=n;i++){
-      printf("%d\n",i);     
-    }
+      if(i%2==0)sum=sum+i;
+      //printf("%d\n",sum);전부 과정을 다 보여줌!   
+    }printf("%d",sum);
 }
 
 //for는(식선언;조건문;변환식){구조}
