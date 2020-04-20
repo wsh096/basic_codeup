0e0ed47b69779e292a00d5ece61fa8f26c21bc4e
wsh096 <wsh096@gmail.com>
2020-04-20 09:48:44 +0000

    "%d", not '%d'!! 1078

--- main.cpp
+++ main.cpp
@@ -9,5 +9,16 @@ int main(){
       //printf("%d\n",sum);전부 과정을 다 보여줌!   
     }printf("%d",sum);
 }
+#include <iostream>
+
+int main(){
+    int sum=0;
+    int i, n;
+    scanf("%d",&n);
+    for(i=0;i<=n;i++){
+    if(i%2==0) sum=sum+i;
+    }printf("%d", sum);
+}
+
 
 //for는(식선언;조건문;변환식){구조}
