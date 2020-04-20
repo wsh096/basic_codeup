7e20e2c686e626a8772ba0154beb6964689be274
wsh096 <wsh096@gmail.com>
2020-04-20 10:00:12 +0000

    1079 훗 레이블 쌓기 if(or while)로 호출

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
 
 int main(){
-    int sum=0;
-    int i, n;
-    scanf("%d", &n);
-    for(i=0;i<=n;i++){
-      if(i%2==0)sum=sum+i;
-      //printf("%d\n",sum);전부 과정을 다 보여줌!   
-    }printf("%d",sum);
-}
-#include <iostream>
-
-int main(){
-    int sum=0;
-    int i, n;
-    scanf("%d",&n);
-    for(i=0;i<=n;i++){
-    if(i%2==0) sum=sum+i;
-    }printf("%d", sum);
+    char x;
+   find:
+  scanf("%c",&x);
+  printf("%c\n",x);
+  if(x!='q') goto find; 
 }
 
 
