c4aa7d6d76e870d21c3efe41ad839683992160e9
wsh096 <wsh096@gmail.com>
2020-04-21 12:14:21 +0000

    1085_MB단위의 값을 구하는 방법!!! 하하 //범위는 크게 해주기!

--- main.cpp
+++ main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 
 int main(){
-    int i,j,k, c=0,r,g,b; 
+    long int h,b,c,s; 
     
-    scanf("%d%d%d",&r,&g,&b);
-    
-    for(i=0;i<r;i++)
-      for(j=0;j<g;j++)
-        for(k=0;k<b;k++)
-        {
-          printf("%d %d %d\n",i, j, k);
-          c++; //C++을 통해서 반복횟수의 경우의 수를 출력할 수 있다!         
-        }printf("%d",c);
+    scanf("%ld%ld%ld%ld",&h,&b,&c,&s);
+    printf("%.1f MB",float(h*b*c*s)/(1024*1024*8));
 }
           //i<r을 통해서 0~n-1까지 구할 수 있어서 n개를 구할 수 있다!
 //빛의 강약에 따라 0 ~ n-1 까지 n가지의 빛 색깔을 만들 수 있다.
-
+//(1024*1024*8)_이게 메가바이트 단위
+///long int로 해야지 메가바이트 정도 단위의 값을 음수로 바뀌지 않고 취급 가능
