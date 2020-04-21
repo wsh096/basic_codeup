267a86ca6208aadbb29bfa37be6cbf1c1ea382ce
wsh096 <wsh096@gmail.com>
2020-04-21 07:46:25 +0000

    1081_주사위 두 개를 던졌을 때? 각 수를 입력받고 경우의 수로 출력 받기!
    응용해서 세 개의 경우의 수
    출력 경우로 만듬

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 
 int main(){
-    int sum=0;
-    int i,n;//합쳐서 int i, n, s=0;으로 써도됨!
-   scanf("%d", &n);
-   for(i=0; sum<n;i++){//sum<n이 없어도 알아서 종료할 시점을 밑의 브레이크가 찾아주므로 여백으로 ;  ;둬도 된다!
-     sum=sum+i;//sum+=i라고 써도 됨
-     if(sum>=n)
-     break;
-     
-     
-   
-   }
-   printf("%d",i);
+    int a, b, c , d, e,f; 
+    
+    scanf("%d%d%d",&d ,&e, &f);
+    
+    for(a=1;a<=d;a++){
+      for(b=1;b<=e;b++){
+        for(c=1;c<=f;c++){
+          printf("%d %d %d\n", a,b,c);
+          }
+        }  
+    }
 }
 
-//if() break;//는 종료 구문이다!
+//for{
+//  for{
+//구조 가능!!
+//}}
 
