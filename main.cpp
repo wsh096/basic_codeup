3f302b3a49a7faa51bfc2e1afbe4cc7122cb0639
wsh096 <wsh096@gmail.com>
2020-04-22 06:06:51 +0000

    1092_최소공배수인 날짜 구하기

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
 int main(){
-    long int a,b,c ,s,i; 
+    int a,b,c, day; 
     
-    scanf("%ld%ld%ld%ld",&a,&b,&c,&s);
-    for(i=1;i<s;i++){////for 반목문에서 가운데의 조건이 빠진 경우 무한 반복된다.
-      a= a*b+c;
+    scanf("%d%d%d",&a,&b,&c);
+    ////for 반목문에서 가운데의 조건이 빠진 경우 무한 반복된다.
+      for(day=1; ;day++){
       
+      if(day%a==0&&day%b==0&&day%c==0)
+      
+      break;
+      }printf("%d ",day);
       //참이면 건너 띄고 반복함
-      
-    }printf("%ld ",a);
+     
 }
