997f825891c84aa6bc00e68dc4b57e498a681615
wsh096 <wsh096@gmail.com>
2020-04-22 05:23:41 +0000

    1088_for의 if에서 contiune;로 건너 띄기

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
 int main(){
-    int a,b,s=0; 
+    int a,b; 
     
     scanf("%d",&b);
-    for(a=1; ;a++){////for 반목문에서 가운데의 조건이 빠진 경우 무한 반복된다.
-      s+=a;
-      if(s>=b)
-      break;// //참이면, 가장 가까운 반복 코드블록의 밖으로 빠져나간다.
-} //break; 가 실행되면 반복을 중단하고 여기로 빠져 나온다.
-    }printf("%d",s);
+    for(a=1;a<=b;a++){////for 반목문에서 가운데의 조건이 빠진 경우 무한 반복된다.
+      if(a%3==0)
+      continue;//참이면 건너 띄고 반복함
+      printf("%d ",a);
+    }
 }
-//
