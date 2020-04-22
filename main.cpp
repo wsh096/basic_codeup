f81c79f2527a409b25d062d0f8eb6d7909f3594a
wsh096 <wsh096@gmail.com>
2020-04-22 05:56:11 +0000

    1091_복잡한 수열의 마지막 숫자 알기 과정을 알고 싶으면 for문 안에 printf넣기

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int main(){
-    long int a,b,s,i; 
+    long int a,b,c ,s,i; 
     
-    scanf("%ld%ld%ld",&a,&b,&s);
+    scanf("%ld%ld%ld%ld",&a,&b,&c,&s);
     for(i=1;i<s;i++){////for 반목문에서 가운데의 조건이 빠진 경우 무한 반복된다.
-      a*=b;
+      a= a*b+c;
       
       //참이면 건너 띄고 반복함
       
